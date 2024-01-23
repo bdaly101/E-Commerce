86c30de7cc4e898171cf0f05689d06df3683fd70
Brendan <daly2b@gmail.com>
2024-01-22 19:13:45 -0500

    Added category routes

--- models/index.js
+++ models/index.js
@@ -4,9 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+})
 
-// Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag
+  },
+  foreignKey: 'product_id',
+})
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag
+  },
+  foreignKey: 'tag_id',
+});
+
+
 
 // Products belongToMany Tags (through ProductTag)
 
